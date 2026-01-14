7c902bf80068c5e9a4c8c3e8a0792e65562a406f
toyota-m2k <toyota.mitsuki@gmail.com>
2026-01-14 14:08:58 +0900

    libs

--- appMediaEditor/build.gradle.kts
+++ appMediaEditor/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.3.0"
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
