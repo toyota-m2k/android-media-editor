41b531c691d010cceffa2d745e3bd3129b6dba7a
toyota-m2k <toyota.mitsuki@gmail.com>
2026-06-02 18:41:47 +0900

    C&C v0.6.0

--- appMediaEditor/build.gradle.kts
+++ appMediaEditor/build.gradle.kts
@@ -35,7 +35,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
