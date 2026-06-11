18b75ee1186fd99c22b76aee125dacc9fedec79f
toyota-m2k <toyota.mitsuki@gmail.com>
2026-06-11 13:50:26 +0900

    update libs

--- appMediaEditor/build.gradle.kts
+++ appMediaEditor/build.gradle.kts
@@ -35,7 +35,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.6.3"
+        versionName = "0.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
